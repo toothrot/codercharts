17c30567602c4841d914ffe5bd961af743fb011d
toothrot <scissorjammer@gmail.com>
2012-06-22 12:03:46 -0500

    never gain

--- fast-grep/fast-grep.c
+++ fast-grep/fast-grep.c
@@ -6,32 +6,28 @@ int main(int argc, char *argv[])
 {
   bool found = false;
   int  o, fp = 0, nl = 1, i = 0;
-  char *map, c;
+  char *map;
 
   fp = open(argv[2], 0);
   map = mmap(NULL, 10000000, PROT_READ, MAP_PRIVATE, fp, 0);
   while(map[i] != 0)
   {
-    c = map[i];
-    if(found)
+    if(c == argv[1][0])
     {
-      o++;
-      if(c != argv[1][o])
+      for(o = 1; argv[1][o] != 0; o++)
       {
-        found = false;
-      } else if(argv[1][o+1] == 0 && map[i+1] == 10)
-      {
-        printf("%i\n", nl);
-        break;
+        if(map[i+o] != argv[1][o])
+          break;
+        if(argv[1][o+1] == 0 && map[i+o+1] == 10)
+        {
+          printf("%i\n", nl);
+          found = true;
+          break;
+        }
       }
-    } else if(c == argv[1][0])
-    {
-      found = true;
-      o = 0;
-      i++;
-      continue;
-    }
-    if(c == 10)
+      if(found)
+        break;
+    } else if(map[i] == 10)
     {
       nl++;
       found = false;
