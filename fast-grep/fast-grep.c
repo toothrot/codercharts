74f2d2efb7c9567f2452b797446791f157608c46
toothrot <scissorjammer@gmail.com>
2012-06-22 11:38:18 -0500

    Better variable names and such

--- fast-grep/fast-grep.c
+++ fast-grep/fast-grep.c
@@ -1,37 +1,44 @@
 #include<stdio.h>
 #include<sys/mman.h>
+#include<stdbool.h>
 
 int main(int argc, char *argv[])
 {
-  unsigned i = 0;
-  int fp = 0, j = -1, nl = 1;
-  char *map;
+  bool found = false;
+  int  o, fp = 0, nl = 1, i = 0;
+  char *map, c;
 
   fp = open(argv[2], 0);
-  map = mmap(NULL, 10000000, PROT_READ, MAP_SHARED, fp, 0);
+  map = mmap(NULL, 10000000, PROT_READ, MAP_PRIVATE, fp, 0);
   while(map[i] != 0)
   {
-    if(map[i] == 10)
+    c = map[i];
+    if(found)
     {
-      nl++;
-      j = 0;
-    } else if(j > 0 || (nl == 1 && j == 0))
-    {
-      if(argv[1][i-j+1] == 0 && map[i+1] == 10)
+      o++;
+      if(c != argv[1][o])
+      {
+        found = false;
+      } else if(argv[1][o+1] == 0 && map[i+1] == 10)
       {
         printf("%i\n", nl);
         break;
-      } else if(map[i] != argv[1][i-j])
-      {
-        j = -1;
       }
-    } else if(map[i] == argv[1][0])
+    } else if(c == argv[1][0])
     {
-      j = i;
+      found = true;
+      o = 0;
+      i++;
+      continue;
+    }
+    if(c == 10)
+    {
+      nl++;
+      found = false;
     }
     i++;
   }
-  if(!j)
+  if(!found)
     printf("0\n");
   munmap(map, 10000000);
   close(fp);
